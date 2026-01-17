c1796a7e84ee7844e781880f1db5f274d11efd97
nazriinaliiyeva <nazriinaliiyeva@gmail.com>
2026-01-17 14:35:01 -0500

    web

--- web_application_security/0x01_owasp_top_10/1-xor_decoder.sh
+++ web_application_security/0x01_owasp_top_10/1-xor_decoder.sh
@@ -1,28 +1,22 @@
 #!/bin/bash
 
-# Check argument
 if [ -z "$1" ]; then
-    echo "Usage: $0 {xor}HASH"
     exit 1
 fi
 
 hash="$1"
-
-# Remove {xor} prefix
 hash="${hash#\{xor\}}"
 
-# Base64 decode to hex
-decoded_hex=$(echo "$hash" | base64 -d | xxd -p)
+# Base64 decode into raw bytes, then output hex using od
+hex=$(echo "$hash" | base64 -d | od -An -tx1 | tr -d ' \n')
 
-# XOR with 0x5A and rebuild string
 result=""
 
-for ((i=0; i<${#decoded_hex}; i+=2)); do
-    byte=${decoded_hex:$i:2}
+for ((i=0; i<${#hex}; i+=2)); do
+    byte=${hex:$i:2}
     xor=$(( 0x$byte ^ 0x5A ))
     result+=$(printf "\\x%02x" $xor)
 done
 
-# Print result
 echo -e "$result"
 
