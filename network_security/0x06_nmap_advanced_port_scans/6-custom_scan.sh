504096d1c5dd380d134b3d3aefb70e3736d36805
nazriinaliiyeva <nazriinaliiyeva@gmail.com>
2026-03-17 07:36:28 -0400

    nmap

--- network_security/0x06_nmap_advanced_port_scans/6-custom_scan.sh
+++ network_security/0x06_nmap_advanced_port_scans/6-custom_scan.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-sudo nmap --scanflags URGACKPSHRSTSYNFIN -p $2 $1 -oN custom_scan.txt > /dev/null 2>&1
+sudo nmap --scanflags URG,ACK,PSH,RST,SYN,FIN -p $2 $1 -oN custom_scan.txt > /dev/null 2>&1
